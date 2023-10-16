306c8f400f95fc956057f0b6098ffe9b2389f3c3
platoabc <101616774+platoabc@users.noreply.github.com>
2023-10-16 23:05:01 +0100

    Add files via upload

--- docs/assets/js/tool-items.js
+++ docs/assets/js/tool-items.js
@@ -101,42 +101,47 @@ function Tool_Items ( deferred )
     
     var modal_default = $( '#popup-item' );
     
-    /*
-    $( '.modal' ).each
+    var hash = window.location.hash.slice( 1 );
+    if ( hash && hash != 'popup-item' ) _util_call_noexcep( function ( ) { $( '#' + hash ).modal( 'show' ) } );
+    
+    $( document ).one
     (
+        'click',
         function ( )
         {
-            var modal = $( this );
-            var id    = modal.attr( 'id' );
-            var hash  = '#' + id;
-            
-            var cb_back = function ( ) { modal.modal( 'hide' ) };
-            
-            modal.on
+            $( '.modal' ).each
             (
-                'shown.bs.modal',
                 function ( )
                 {
-                    window.history.pushState( hash, id, window.location.pathname + window.location.search + hash );
-                    window.addEventListener( 'popstate', cb_back, false );
-                }
-            );
-            
-            modal.on
-            (
-                'hidden.bs.modal',
-                function ( )
-                {
-                    if ( window.history.state === hash ) window.history.go( -1 );
-                    window.removeEventListener( 'popstate', cb_back );
+                    var modal = $( this );
+                    var id    = modal.attr( 'id' );
+                    var hash  = '#' + id;
+                    
+                    var cb_back = function ( ) { modal.modal( 'hide' ) };
+                    
+                    modal.on
+                    (
+                        'shown.bs.modal',
+                        function ( )
+                        {
+                            window.history.pushState( hash, id, window.location.pathname + window.location.search + hash );
+                            window.addEventListener( 'popstate', cb_back, false );
+                        }
+                    );
+                    
+                    modal.on
+                    (
+                        'hidden.bs.modal',
+                        function ( )
+                        {
+                            if ( window.history.state === hash ) window.history.go( -1 );
+                            window.removeEventListener( 'popstate', cb_back );
+                        }
+                    );
                 }
             );
         }
     );
-    */
-    
-    var hash = window.location.hash.slice( 1 );
-    if ( hash && hash != 'popup-item' ) _util_call_noexcep( function ( ) { $( '#' + hash ).modal( 'show' ) } );
     
     // datatable
     
