cc21ca5c285687bebb1d1c45745945db6f32f1c2
platoabc <101616774+platoabc@users.noreply.github.com>
2023-10-19 22:51:38 +0100

    Add files via upload

--- docs/assets/js/util.js
+++ docs/assets/js/util.js
@@ -244,6 +244,7 @@ function _util_download ( url, name = '' )
     var link = document.createElement( 'a' );
     link.download = name;
     link.href = url;
+    link.target = '_blank';
     document.body.appendChild( link );
     link.click( );
     document.body.removeChild( link );
